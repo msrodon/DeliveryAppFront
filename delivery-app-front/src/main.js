6b9f99ae4b894b71eb4dd35a5c58ac08382576c0
Mariusz Środoń <msrodon@9bits.pl>
2024-09-08 16:10:21 +0200

    setup routing

--- delivery-app-front/src/main.js
+++ delivery-app-front/src/main.js
@@ -1,6 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import './registerServiceWorker'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+const app = createApp (App)
+app.use(router)
+app.use(VueAxios, axios)
+app.mount('#app')
+
+// import "bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+// app.config.globalProperties.hostname="https://localhost:44354"
--- delivery-app-front/src/router/index.js
+++ delivery-app-front/src/router/index.js
@@ -1,5 +1,10 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from "../components/Login.vue"
+import Register from "../components/Register.vue"
+
+const Dashboard = () => import("../components/Dashboard.vue")
+const NotFound = () => import("../components/404-page.vue")
 
 const routes = [
   {
@@ -16,12 +21,54 @@ const routes = [
     component: function () {
       return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
     }
+  },
+  {
+      path: "/dashboard",
+      name: "dashboard",
+      component: Dashboard,
+  },
+  {
+      path: "/404",
+      name: "404-page",
+      component: NotFound,
+  },
+  {
+      path: "/register",
+      name: "register",
+      component: Register,
+  },
+  {
+      path: "/login",
+      name: "login",
+      component: Login,
   }
 ]
 
+// const router = createRouter({
+//   history: createWebHashHistory(),
+//   routes
+// })
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
+  history: createWebHistory(),
+  routes,
+  });
+
+  router.beforeEach((to, from, next) => {
+      const publicPages = ['/login', '/register'];
+      const authRequired = !publicPages.includes (to.path);
+      const loggedIn = localStorage.getItem("token");
+      
+      if (authRequired && !loggedIn) {
+          next('/login');
+      } else {
+          next();
+      }
+
+      if (authRequired) {
+          next('/login');
+      } else {
+          next();
+      }
+  });
 
 export default router
