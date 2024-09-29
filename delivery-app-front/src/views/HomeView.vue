cb9e76fbaf4940962cb353b87c94a7e251f121e1
Mariusz Środoń <msrodon@9bits.pl>
2024-09-29 20:56:56 +0200

    Remove unused view

--- delivery-app-front/src/views/HomeView.vue
+++ delivery-app-front/src/views/HomeView.vue
@@ -1,17 +1,51 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-10 col-lg-10 col-xl-8">
+        <div class="card bg-white text-dark" style="border-radius: 1rem;">
+          <div class="card-body p-5 text-center">
+
+            <div class="mb-md-5 mt-md-4 pb-5">
+
+              <h2 class="fw-bold mb-2 text-uppercase">WELCOME TO MAIN PAGE...</h2>
+              <p class="text-dark-50 m-0">na gurze rurze</p>
+              <p class="text-dark-50 m-0">kuce w piwnicy</p>
+              <hr>
+              <img src="../assets/eyes1.png" alt="" width="20%">
+
+              <p class="text-dark-50 m-0">spacje czy taby</p>
+              <p class="text-dark-50 m-0">nie widze różnicy</p>
+              
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  name: 'HomeView'
 }
 </script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
