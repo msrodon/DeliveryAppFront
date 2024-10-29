0a71acf71328f9d3040340fabaefc6a3bad1509d
Mariusz Środoń <msrodon@9bits.pl>
2024-10-29 21:50:06 +0100

    Add Boostrap5 + Transfer NavBar

--- delivery-app-front/src/components/NavBar.vue
+++ delivery-app-front/src/components/NavBar.vue
@@ -1,36 +1,59 @@
 <template>
-    <div>
-      <b-navbar class="px-3">
-        <b-navbar-nav>
-          <div class="d-flex pe-3">
-            <a href="/"><b-icon-house scale="2" variant="light" shift-v="-8"></b-icon-house></a>
-          </div>
-          <b-nav-item-dropdown text="MENU" right>
-            <b-dropdown-item href="/TestEndpoints">Testowe przyciski</b-dropdown-item>
-            <b-dropdown-item href="/404">404 page</b-dropdown-item>
-          </b-nav-item-dropdown>
-
-          <b-nav-item-dropdown text="AUTH" right>
-            <b-dropdown-item href="/Login">LOGIN</b-dropdown-item>
-            <b-dropdown-item href="/Register">REJESTRACJA</b-dropdown-item>
-          </b-nav-item-dropdown>
-
-          <b-nav-item-dropdown text="USERS" right>
-            <b-dropdown-item href="/Users">Users list</b-dropdown-item>
-          </b-nav-item-dropdown>
-
-          <b-nav-item-dropdown text="CARS" right>
-            <b-dropdown-item href="/Cars">Cars list</b-dropdown-item>
-            <b-dropdown-item href="/Cars/AddCar">Add car</b-dropdown-item>
-          </b-nav-item-dropdown>
-          
-          <b-nav-item-dropdown text="DICTIONARIES" right>
-            <b-dropdown-item href="/DictionaryTypes">Dictionaries types</b-dropdown-item>
-          </b-nav-item-dropdown>
-
-        </b-navbar-nav>
-      </b-navbar>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">HOME</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              MENU
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+              <li><a class="dropdown-item" href="/TestEndpoints">TEST BUTTONS</a></li>
+              <li><a class="dropdown-item" href="/404">404 PAGE</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              AUTH
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+              <li><a class="dropdown-item" href="/Login">LOGIN</a></li>
+              <li><a class="dropdown-item" href="/Register">REGISTER</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              USERS
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+              <li><a class="dropdown-item" href="/Users">USERS LSIT</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              CARS
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+              <li><a class="dropdown-item" href="/Cars">CARS LSIT</a></li>
+              <li><a class="dropdown-item" href="/Cars/AddCar">ADD CAR</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              DICTIONARIES
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+              <li><a class="dropdown-item" href="/DictionaryTypes">DICTIONARY TYPES</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
+  </nav>
 </template>
 
 <style lang="scss">
